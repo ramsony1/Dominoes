4be06a9f0d188c6ba7cbaf818cb235a1ae69f00b
Ramadhani Mdee <42729286+ramsony@users.noreply.github.com>
2021-07-12 23:44:47 -0400

    Select a domino and place it on the right side of the snake. Select a domino and place it on the left side of the snake. Take an extra piece from the stock (if it's not empty) and skip a turn.

--- dominoes.py
+++ dominoes.py
@@ -1,3 +1,4 @@
+import itertools
 import random
 
 snake, stock, player, computer = [], [], [], []
@@ -6,10 +7,10 @@ status = ""
 
 def starter_piece():
     computer_player = computer + player
-    doubles = [piece for piece in computer_player if piece[0] == piece[1]]
-    if not doubles:
+    double_piece = [piece for piece in computer_player if piece[0] == piece[1]]
+    if not double_piece:
         return False
-    snake.append(max(doubles))
+    snake.append(max(double_piece))
     if snake[0] in computer:
         computer.remove(snake[0])
         return "player"
@@ -23,17 +24,53 @@ while not status:
     computer, player, stock = domino_pieces[:7], domino_pieces[7:14], domino_pieces[14:]
     status = starter_piece()
 
-print("=" * 70)
-print(f"Stock size: {len(stock)}")
-print(f'Computer size: {len(computer)}\n')
-print(snake[0])
-print("\nYour pieces:")
-n = 1
-for i in player:
-    print(f'{n}:{i}')
-    n += 1
-if status == "player":
-    print("Status: It's your turn to make a move. Enter your command.")
-else:
-    print("Status: Computer is about to make a move. Press Enter to continue...")
 
+def display_output():
+    global n, i
+    print("=" * 70)
+    print(f"Stock size: {len(stock)}")
+    print(f'Computer piece: {len(computer)}\n')
+    print({True: f"{snake[:3]}...{snake[-3:]}", False: snake}[len(snake) > 6])
+
+    print("\nYour pieces:")
+    n = 1
+    for i in player:
+        print(f'{n}:{i}')
+        n += 1
+
+
+display_output()
+
+while True:
+    if status == "player":
+        move = input("Status: It's your turn to make a move. Enter your command.")
+        try:
+            move_digit = int(move)
+        except ValueError:
+            while True:
+                move = input("Invalid input. Please try again.")
+                try:
+                    move_digit = int(move)
+                    break
+                except ValueError:
+                    pass
+        if move_digit < 0:
+            snake = [player[abs(move_digit) - 1]] + snake
+            player.remove(player[abs(move_digit) - 1])
+            display_output()
+            status = "computer"
+        elif move_digit == 0:
+            player = player + [stock[0]]
+            stock.remove(stock[0])
+            display_output()
+            status = "computer"
+        else:
+            snake = snake + [player[move_digit - 1]]
+            player.remove(player[move_digit - 1])
+            display_output()
+            status = "computer"
+
+    else:
+        move = input("Status: Computer is about to make a move. Press Enter to continue...")
+        display_output()
+        status = "player"
